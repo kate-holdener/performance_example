4809836b4d1f93b53747f0cc80ae7f5b8150f4bb
Kate Holdener <kate.holdener@slu.edu>
2023-04-26 08:50:09 -0500

    Adjusted the script to accept parameters

--- run_fib.sh
+++ run_fib.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-TIMEFORMAT=%R
+TIMEFORMAT=%R,%U,%S
 n=(1 10 20 30 40)
+scriptToTest=$1
+outputFile=$2
+
 for i in ${n[@]}
 do
    num_samples=10
    while [[ $num_samples -gt 0 ]]
    do
-      run_time=$( (time python3 fib.py ${i} > /dev/null) 2>&1)
+      run_time=$( (time python3 $scriptToTest ${i} > /dev/null) 2>&1)
       echo $run_time
-      echo "$i, $run_time" >> performance_fib.csv
+      echo "$i,$run_time" >> $outputFile
       echo $run_time
       num_samples=($num_samples-1)
    done
